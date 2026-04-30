3522fe598d273022ed4ac9b18d1c01af84696fcf
Costacu <stefan.costache.8@gmail.com>
2026-04-30 13:49:00 +0300

    Landing page and login page frontend added

---
+++ frontend/src/app/home/home.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router'; // <--- Essential for buttons to work
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  // You must import RouterLink here so the HTML knows what routerLink="/login" means
+  imports: [CommonModule, RouterLink], 
+  templateUrl: './home.html',
+  styleUrls: ['./home.css']
+})
+export class HomeComponent {
+  // Logic for the home page goes here (if any)
+
+
+  //Test comment lmao
+  // Another test comment lmao why doesn't github link to my account 
+}
